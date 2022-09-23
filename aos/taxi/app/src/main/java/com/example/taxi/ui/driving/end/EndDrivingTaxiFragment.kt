77a4d3627950ae1e9156168fbd727c9a59f06650
DESKTOP-KVCQHCD\multicampus <mhs5594@naver.com>
2022-09-23 15:04:09 +0900

    [S07P22D208-120 | AOS] feat: EndDrivingTaxi 구현

--- aos/taxi/app/src/main/java/com/example/taxi/ui/driving/end/EndDrivingTaxiFragment.kt
+++ aos/taxi/app/src/main/java/com/example/taxi/ui/driving/end/EndDrivingTaxiFragment.kt
@@ -1,14 +1,39 @@
 package com.example.taxi.ui.driving.end
 
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.taxi.R
 import com.example.taxi.base.BaseFragment
 import com.example.taxi.databinding.FragmentEndDrivingTaxiBinding
+import com.example.taxi.di.ApplicationClass
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EndDrivingTaxiFragment : BaseFragment<FragmentEndDrivingTaxiBinding>(R.layout.fragment_end_driving_taxi) {
+
     override fun init() {
-        TODO("Not yet implemented")
+        initData()
+        setOnClickListeners()
+    }
+
+    private fun initData() {
+        if(ApplicationClass.prefs.carImage != ""){
+            Glide.with(this).load(ApplicationClass.prefs.carImage).into(binding.imageEndTaxiCar)
+        }
+        binding.textEndTaxi.text = ApplicationClass.prefs.carNumber
+    }
+
+    private fun setOnClickListeners(){
+
+        binding.buttonEndTaxiPhoto.setOnClickListener {
+            findNavController().navigate(R.id.action_endDrivingTaxiFragment_to_drivingTaxiCheckFragment)
+        }
+        binding.buttonEndTaxiStart.setOnClickListener {
+            //사진 다 넣었는지 확인하기
+            //TODO : RATINGBAR 값 업데이트
+            //TODO : 후불일때 처리
+            findNavController().navigate(R.id.action_endDrivingTaxiFragment_to_userHomeFragment)
+        }
     }
 
 }
